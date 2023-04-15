ba0e4f4f32b5c8efbc13c4a0e6da7c10e5b7471b
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2023-04-15 12:49:25 +0530

    Update builder_state.go

--- builder/builder_state.go
+++ builder/builder_state.go
@@ -3,7 +3,7 @@ package builder
 import (
 	"context"
 
-	"github.com/datainfrahq/operator-builder/utils"
+	"github.com/datainfrahq/operator-runtime/utils"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
